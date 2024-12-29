ee3b04b7d59c0f35b55f8a8a5a33649c3b19d42a
Matej Dedina <gameplay.nano@gmail.com>
2024-12-29 23:22:10 +0100

    testing bsearch function

--- headers/binary_set.h
+++ headers/binary_set.h
@@ -166,7 +166,7 @@ static inline void destroy_binary_set(binary_set_s * set, const destroy_binary_s
 
 static inline bool contains_binary_set(const binary_set_s set, const BINARY_SET_DATA_TYPE element) {
     if (set.compare) { // uses buildin binary search implmentation if compare function is specified
-        return (bool) bsearch(&element, set.elements, set.size, sizeof(BINARY_SET_DATA_TYPE), set.compare);
+        return bsearch(&element, set.elements, set.size, sizeof(BINARY_SET_DATA_TYPE), set.compare) != NULL;
     }
     // else custom binary search is used as memcmp takes three arguments as input
     // (bsearch requires a 2 argument compare function). IF ONLY NESTED FUNCTION WERE PART OF THE C STANDARD.
